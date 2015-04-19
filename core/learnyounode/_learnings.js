12de5bed19d27ea2022868ba9ed059a643614e67
filtercake <fabian@superpursuitmo.de>
2015-04-19 11:38:54 +0200

    initial learnyounode lessons and learningsfile

---
+++ core/learnyounode/_learnings.js
@@ -0,0 +1,9 @@
+// process arguments
+
+// process.argv is an array
+
+console.log(process.argv)
+// 0: node
+// 1: name of program
+// 2+ arguments
+// arguments are strings, convert with Number(process.argv[i])
